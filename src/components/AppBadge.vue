d8beb2b1b3c002463f45bd309a85ffc1f3b7bd68
Ertuğrul Elibol <ai.wxby@gmail.com>
2023-06-10 02:18:06 +0300

    add prop class to elements

--- src/components/AppBadge.vue
+++ src/components/AppBadge.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
-    <p>Development</p>
+  <div
+    class="inline-block rounded-md px-3 py-1.5 bg-gray-100"
+    :class="wrapperClass"
+  >
+    <p class="font-bold text-sm text-gray-600" :class="textClass">
+      Development
+    </p>
   </div>
 </template>
 
 <script>
 export default {
   name: "AppBadge",
+  props: {
+    textClass: String,
+    wrapperClass: String,
+  },
 };
 </script>
