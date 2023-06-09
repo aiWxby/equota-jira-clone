05c2d2ea8980cf456370e5c64f6e03ba00f2357b
Ertuğrul Elibol <ai.wxby@gmail.com>
2023-06-10 01:16:56 +0300

    add elements to alert component

--- src/components/AppAlert.vue
+++ src/components/AppAlert.vue
@@ -1,9 +1,16 @@
 <template>
-  <div></div>
+  <div class="p-4 rounded-t-md bg-primary-100 border-b-4 border-primary-500 text-left">
+    <p class="text-primary-600">
+      {{ alertMessage }}
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
   name: "AppAlert",
+  props: {
+    alertMessage: String
+  }
 };
 </script>
