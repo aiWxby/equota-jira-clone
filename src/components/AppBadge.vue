6c1be17fd36997c2b98476dc885b276eaefaf78b
Ertuğrul Elibol <ai.wxby@gmail.com>
2023-06-11 01:09:26 +0300

    refactor: replace tailwind classes with specific classes

--- src/components/AppBadge.vue
+++ src/components/AppBadge.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="inline-block rounded-md px-3 py-1.5 bg-gray-100"
+    class="badge"
     :class="wrapperClass"
   >
-    <p class="font-bold text-sm text-gray-600" :class="textClass">
+    <p class="badge_content" :class="textClass">
       {{ content }}
     </p>
   </div>
