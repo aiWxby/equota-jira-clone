bf2f9f211f09b24c800363ce169d7f19a6304228
Ertuğrul Elibol <ai.wxby@gmail.com>
2023-06-11 01:33:56 +0300

    refactor: add dynamic props & data

--- src/components/TaskSection.vue
+++ src/components/TaskSection.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="w-80 flex flex-col bg-purple-200 space-y-8">
-    <h3 class="text-[15px] font-bold text-gray">TO DO</h3>
-    <div class="flex flex-col space-y-6"></div>
+  <div class="w-80 flex flex-col space-y-8" :class="wrapperClass">
+    <h3 class="text-[15px] font-bold text-gray" :class="titleClass">
+      {{ sectionTitle }}
+    </h3>
+    <div class="flex flex-col space-y-6" :class="taskListClass"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: "TaskSection",
+  props: {
+    sectionTitle: String,
+    titleClass: String,
+    wrapperClass: String,
+    taskListClass: String,
+  },
 };
 </script>
