b1ae31795f8756bd9bf92b705b159d5b08882369
Ertuğrul Elibol <ai.wxby@gmail.com>
2023-06-10 17:29:56 +0300

    add prop class binding option

--- src/components/SideBarMenuListItem.vue
+++ src/components/SideBarMenuListItem.vue
@@ -1,10 +1,13 @@
 <template>
   <button
     class="flex items-center justify-between py-3 px-2 rounded-lg hover:bg-gray-100 transition-colors duration-300"
+    :class="wrapperClass"
   >
     <div class="flex items-center space-x-3">
-      <slot name="icon"></slot>
-      <p class="text-gray-800 font-bold text-base">Home</p>
+      <slot name="icon" :class="iconClass"></slot>
+      <p class="text-gray-800 font-bold text-base" :class="contentClass">
+        {{ content }}
+      </p>
     </div>
     <!--
       TODO: Bu kısma badge gelecek. Props'lar ile kontrol edilecek ve ona göre içine değer yazdırılacak.
@@ -15,5 +18,12 @@
 <script>
 export default {
   name: "SideBarMenuListItem",
+  props: {
+    wrapperClass: String,
+    iconClass: String,
+    content: String,
+    contentClass: String,
+    badgeClass: String,
+  },
 };
 </script>
