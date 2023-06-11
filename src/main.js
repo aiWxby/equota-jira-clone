16c5356c9d7914a9ff5c914dfabd96f62bc45f7b
Ertuğrul Elibol <ai.wxby@gmail.com>
2023-06-11 18:36:01 +0300

    import pinia to main.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./assets/tailwind.css";
 import router from "@/router"
+import pinia from "@/stores"
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  pinia,
   render: (h) => h(App),
 }).$mount("#app");
