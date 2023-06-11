1426d0f35c0a957ac0bc1c33e66dc433b44128db
Ertuğrul Elibol <ai.wxby@gmail.com>
2023-06-11 18:58:07 +0300

    importing related components & store related files & looping data

--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -1,11 +1,32 @@
 <template>
-  <div
-    class="h-full w-full flex overflow-x-auto overflow-y-hidden space-x-9"
-  ></div>
+  <div class="h-full w-full flex overflow-x-auto overflow-y-hidden space-x-9">
+    <template v-for="(item, index) in taskData">
+      <task-section :sectionTitle="item.sectionTitle" :key="index">
+        <template v-for="(task, index) in taskData[index].tasks">
+          <TaskCard :task="task" :key="index" />
+        </template>
+      </task-section>
+    </template>
+    <task-section sectionTitle="test">
+      <template v-for="(task, index) in tasksOfSection">
+        <TaskCard :task="task" :key="index" />
+      </template>
+    </task-section>
+  </div>
 </template>
 
 <script>
+import { useTaskStore } from "@/stores/TaskStore";
+import { mapState } from "pinia";
+
+import TaskSection from "@/components/TaskSection.vue";
+import TaskCard from "@/components/TaskCard.vue";
+
 export default {
   name: "TasksView",
+  components: { TaskSection, TaskCard },
+  computed: {
+    ...mapState(useTaskStore, { taskData: "getTaskData" }),
+  },
 };
 </script>
