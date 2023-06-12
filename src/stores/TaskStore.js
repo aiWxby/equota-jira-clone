affe8b6f77e9360d4cd73ce78d09ace1e23939a4
Ertuğrul Elibol <ai.wxby@gmail.com>
2023-06-12 03:49:54 +0300

    refactor: task store

--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -1,13 +1,52 @@
 import { defineStore } from "pinia";
-import { taskData } from "@/data/taskData";
+import { taskSections, taskList } from "@/data/taskData";
 
 export const useTaskStore = defineStore("TaskStore", {
   state: () => {
     return {
-      taskData,
+      taskSections,
+      taskList,
     };
   },
   getters: {
-    getTaskData: (state) => state.taskData,
+    getTaskSections: (state) => state.taskSections,
+    getTaskSectionsIDs: (state) =>
+      state.taskSections.map((section) => section.id),
+    getTaskList: (state) => state.taskList,
+    getTaskCount: (state) => state.taskList.length,
+  },
+  actions: {
+    updateTaskSectionName(index, title, id) {
+      this.taskSections[index].sectionTitle = title.toUpperCase();
+      this.taskSections[index].id = id;
+    },
+    addTaskSection() {
+      this.taskSections.unshift({
+        id: "random",
+        sectionTitle: "RANDOM",
+      });
+    },
+    addTask(whereTo) {
+      this.taskList.push({
+        belongsTo: whereTo,
+        title: "Tebrikler! Rastgele bir task ürettiniz",
+        description:
+          "Gönül daha detaylı bir işlem yapmak isterdi fakat kaygılar çok başkaydı...",
+        date: "Zamansız",
+        author: "Ertuğrul",
+        image: "",
+        files: [],
+        tags: [
+          {
+            palette: "gray",
+            content: "Test",
+          },
+        ],
+        comments: {
+          count: 0,
+          avatars: [],
+        },
+      });
+    },
   },
 });
